0b4cf2976cf1c89b6cf38e47ace5437d63c015d9
yrwnds <l.zanini.nunes@gmail.com>
2026-06-09 20:32:47 -0300

    fix para flutter

--- src/main/java/br/csi/bibliotecadigital/model/usuario/DadosUsuario.java
+++ src/main/java/br/csi/bibliotecadigital/model/usuario/DadosUsuario.java
@@ -3,7 +3,7 @@ package br.csi.bibliotecadigital.model.usuario;
 public record DadosUsuario(Long id, String matricula, String identificador, String email, String nome, String senha, String imagePath) {
 
     public DadosUsuario(Usuario usuario){
-        this(usuario.getId(),usuario.getMatricula(),usuario.getIdentificador(), usuario.getEmail(), usuario.getNome(), usuario.getSenha(), usuario.getImagePath());
+        this(usuario.getId(),usuario.getMatricula(),usuario.getIdentificador(), usuario.getEmail(), usuario.getNome(), usuario.getSenha(), usuario.getImagem_path());
     }
 
 }
